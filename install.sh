71c53e5ed115855db9343a718f701af0a4ab0819
Jaat-Hacker <jaatchanderbhanchoudhary@gmail.com>
2025-04-07 14:38:25 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -57,6 +57,8 @@ rainbow_type() {
 # Rainbow animated subtitle
 rainbow_type "Instagram Phishing Tool for Ethical Hacking Practice" 0.03
 rainbow_type "Created by: SACHIN BADASRA" 0.04
+echo ""
+}
 
 # Colors
 GRN='\033[0;32m'
